926e44873b3dc9d6c02a06c84713e543f086ea18
dinhphuochiep <dinhphuochiep1987@gmail.com>
2014-03-17 10:22:48 +0700

    model CacKhoanTru

---
+++ app/assets/js/model/CacKhoangTru.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dungvn3000 on 3/17/14.
+ */
+Ext.define('sunerp.model.CacKhoanTru', {
+    extend: 'sunerp.model.BaseModel',
+    fields: [
+        'id',
+        'doanPhi',
+        'ungKy1',
+        'bhyt',
+        'bhxh',
+        'thuNo'
+    ]
+});
