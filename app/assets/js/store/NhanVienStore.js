263fd0e6e24909c88f7325664cd0376bf4b0c34d
dinhphuochiep <dinhphuochiep1987@gmail.com>
2014-03-17 11:36:23 +0700

    store NhanVienStore

---
+++ app/assets/js/store/NhanVienStore.js
@@ -0,0 +1,11 @@
+/**
+ * Created by dungvn3000 on 3/17/14.
+ */
+Ext.define('sunerp.store.NhanVienStore', {
+    extend: 'sunerp.store.BaseStore',
+    model: 'sunerp.model.NhanVien',
+    proxy: {
+        type: 'restx',
+        url: '/nhanvien'
+    }
+});
