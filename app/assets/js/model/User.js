44e12e105eecf9fbefdb2794cf803eee7343a86f
dinhphuochiep <dinhphuochiep1987@gmail.com>
2014-03-17 10:08:38 +0700

    sửa user model

--- app/assets/js/model/User.js
+++ app/assets/js/model/User.js
@@ -1,13 +1,17 @@
 Ext.define('sunerp.model.User', {
     extend: 'sunerp.model.BaseModel',
     fields: [
+        'id',
         'username',
         'password',
         'roleId',
+        'nhanVienId',
         'role',
+        'nhanVien',
         {name: 'role.name', mapping: 'role.name'}
     ],
     associations: [
-        {type: 'belongsTo', model: 'sunerp.model.Role', name: 'role'}
+        {type: 'belongsTo', model: 'sunerp.model.Role', name: 'role'},
+        {type: 'belongsTo', model: 'sunerp.model.NhanVien', name: 'nhanVien'}
     ]
 });
