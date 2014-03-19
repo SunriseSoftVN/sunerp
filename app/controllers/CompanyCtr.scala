80522bf86071744f971b7f435f6834c99b7f7afe
dungvn3000@gmail.com <ducdung2512>
2014-03-19 08:25:36 +0700

    Add company all router.

--- app/controllers/CompanyCtr.scala
+++ app/controllers/CompanyCtr.scala
@@ -24,4 +24,7 @@ object CompanyCtr extends BaseCtr[Company, Companies] with MainTemplate {
     val result = Companies.load(paging)
     Json.toJson(result)
   }
+  def all = StackAction(AuthorityKey -> domainName)(implicit request => {
+    Ok(Json.toJson(Companies.all))
+  })
 }
