042a985e7e731616d75d34d4d62998881fd86422
dungvn3000@gmail.com <ducdung2512>
2014-04-10 15:21:18 +0700

    Implement TextFilter.

--- app/assets/js/component/core/BaseGirdFilter.js
+++ app/assets/js/component/core/BaseGirdFilter.js
@@ -3,5 +3,15 @@
  */
 
 Ext.define('sunerp.component.core.BaseGirdFilter', {
-
+    comp: null,
+    filter: null,
+    constructor: function () {
+        this.callParent(arguments);
+    },
+    getFilter: function () {
+        return this.filter;
+    },
+    getComponent: function () {
+        return this.comp;
+    }
 });
--- app/assets/js/component/filter/TextFilter.js
+++ app/assets/js/component/filter/TextFilter.js
@@ -3,5 +3,18 @@
  */
 
 Ext.define('sunerp.component.filter.TextFilter', {
-    extend: 'sunerp.component.core.BaseGirdFilter'
+    extend: 'sunerp.component.core.BaseGirdFilter',
+    fieldName: null,
+    constructor: function () {
+        var me = this;
+        if (me.comp) {
+            var value = me.comp.getValue();
+            var filter = new Ext.util.Filter({
+                property: me.fieldName,
+                value: value
+            });
+            me.filter = filter;
+        }
+        me.callParent(arguments);
+    }
 });
