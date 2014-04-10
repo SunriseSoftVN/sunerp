2d823b174e5bcec9758566801689342a0689f343
dungvn3000@gmail.com <ducdung2512>
2014-04-10 14:46:21 +0700

    Add BaseGirdFilter & TextFilter & ComboboxFilter

---
+++ app/assets/js/component/core/BaseGirdFilter.js
@@ -0,0 +1,7 @@
+/**
+ * Created by dungvn3000 on 4/10/14.
+ */
+
+Ext.define('sunerp.component.core.BaseGirdFilter', {
+
+});
---
+++ app/assets/js/component/filter/ComboboxFilter.js
@@ -0,0 +1,7 @@
+/**
+ * Created by dungvn3000 on 4/10/14.
+ */
+
+Ext.define('sunerp.component.filter.ComboboxFilter', {
+    extend: 'sunerp.component.core.BaseGirdFilter'
+});
