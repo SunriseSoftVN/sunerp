c0bc646afa262df69a8b7d92f189a34a84af4d97
dungvn3000@gmail.com <ducdung2512>
2014-04-11 11:47:33 +0700

    I18n month names.

--- app/assets/js/Application.js
+++ app/assets/js/Application.js
@@ -36,6 +36,21 @@ Ext.define('sunerp.Application', {
         Deft.promise.Deferred.enableLogging = false;
         Ext.select("#loading-container").hide();
         Ext.tip.QuickTipManager.init();
+        Ext.Date.monthNames = [
+            "T1",
+            "T2",
+            "T3",
+            "T4",
+            "T5",
+            "T6",
+            "T7",
+            "T8",
+            "T9",
+            "T10",
+            "T11",
+            "T12"
+        ];
+
         return Ext.create("sunerp.view.Viewport");
     },
 
